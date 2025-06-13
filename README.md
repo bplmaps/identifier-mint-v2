13f1adc6de90677ed5978496c2f8baea84b8092e
Ian Spangler <39773984+itspangler@users.noreply.github.com>
2025-06-13 14:25:17 -0400

    add readme

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # `identifier-mint-v2`
 
-Version 2 of LMEC's Identifier Mint tool for minting new spatial data records in our ARK namespace. Built in SvelteKit.
+Version 2 of LMEC's Identifier Mint tool for minting new spatial data records in our ARK namespace. Built in SvelteKit. Requires `node` >=18.13.
 
 ## Develop locally
 
 1. Clone this repository and `cd` into it
 2. `npm install`
 3. `npm run dev`
+
+## Deploy to Wasabi
+
+This app is currently deployed to Wasabi as a static site.
+
+To "deploy," we run `npm run build` from the root. The `build` directory should update (you can always delete this folder before you `npm run build` to make sure it's updating).
+
+Then, hand-copy the contents of `build` to `var/www/geoservices/identifier-mint` Wasabi.
+
+> SvelteKit provides documentation on deploying apps as a static site: https://svelte.dev/docs/kit/adapter-static
+
+#### Setting up the static site deployment is only something we should need to do *one time*. For posterity, this is how I did it:
+
+1. Install `@sveltejs/adapter-static`
+2. Update the `svelte.config.js` according to SvelteKit docs
+3. Create a file called `+layout.ts` inside of `/routes`
--- svelte.config.js
+++ svelte.config.js
@@ -13,39 +13,3 @@ export default {
 		})
 	}
 };
-
-// import adapter from '@sveltejs/adapter-auto';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	// Consult https://svelte.dev/docs/kit/integrations
-// 	// for more information about preprocessors
-	// preprocess: vitePreprocess(),
-
-// 	kit: {
-// 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-// 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-// 		adapter: adapter()
-// 	}
-// };
-
-// import adapter from '@sveltejs/adapter-static';
-
-// export default {
-// preprocess: vitePreprocess(),
-//   kit: {
-//     adapter: adapter({
-//       pages: 'build',
-//       assets: 'build',
-//       fallback: null // or '200.html' if using SPA fallback
-//     }),
-//     paths: {
-//       base: '/my-app' // IMPORTANT: matches your deployment path
-//     },
-//     prerender: {
-//       entries: ['*'] // prerender all reachable pages
-//     }
-//   }
-// };
